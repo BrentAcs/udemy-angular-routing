78d26377c63d4917b9ae23bfb7a0040c26bbe318
Brent Acs <brent.mann@gmail.com>
2021-02-15 16:23:03 -0500

    EoL 11.143 Added redirection in routing and wildcard route to catch all 'not found' routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -23,6 +24,10 @@ const appRoutes: Routes = [
       { path: ":id/edit", component: EditServerComponent },
     ],
   },
+  { path: "not-found", component: PageNotFoundComponent},
+
+  // NOTE: '**' is wildcard catch-all and should always be last entry in routing list.
+  { path: "**", redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -33,7 +38,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
